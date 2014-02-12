341a9205e7196156464b8e94120a525e4e87a534
evangilo <evangilo@gmail.com>
2014-02-12 19:30:57 -0300

    add users

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -3,8 +3,12 @@ import lionmessage.User
 class BootStrap {
 
     def init = { servletContext ->
-		def user = new User(lionname: "leosilva", username: "Leo Silva", password: "123")
-		user.save(flush: true)
+		def user0 = new User(lionname: "leosilva", username: "Leo Silva", password: "123")
+		def user1 = new User(lionname: "evangilo", username: "evangilo", password: "123")
+		def user2 = new User(lionname: "jeff", username: "jeff", password: "321")
+		user0.save(flush: true)
+		user1.save(flush: true)
+		user2.save(flush: true)
     }
     def destroy = {
     }
